6f8c4a80fc7c446283253bd2fe72cbe07cf98b8c
Javi <trarsheise@gmail.com>
2024-05-26 19:54:58 +0200

    playing with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, docData } from '@angular/fire/firestore';
+import { DocumentData, DocumentReference, doc, updateDoc } from '@firebase/firestore';
+import { Observable, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+
+interface App {
+  counter: number
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatProgressSpinnerModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'club';
+  ready = false;
+  counter = 0;
+  docRef: DocumentReference<DocumentData, DocumentData>
+
+  constructor(private firestore: Firestore) {
+    this.docRef = doc(this.firestore, 'app', 'settings');
+    const doc$ = docData(this.docRef) as Observable<App>;
+    doc$.subscribe((doc) => {
+      this.counter = doc.counter
+      this.ready = true
+    });
+
+  }
+
+  sum() {
+    this.counter++;
+    updateDoc(this.docRef, { counter: this.counter })
+  }
+
+  reset() {
+    this.counter = 0;
+    updateDoc(this.docRef, { counter: this.counter })
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"club-juegos-e57b9","appId":"1:652977867113:web:c31aeaf7bd14a0516255f8","storageBucket":"club-juegos-e57b9.appspot.com","apiKey":"AIzaSyDb6GTzso0Pls6eXdzmcKYEU74t2UcqYVs","authDomain":"club-juegos-e57b9.firebaseapp.com","messagingSenderId":"652977867113"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
